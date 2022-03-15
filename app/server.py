511cb0c0f6e5c816b025716ab200955d35540f79
kshtijdhara <imkshitijdhara@gmail.com>
2022-03-15 16:55:45 +0530

    added base html page

--- app/server.py
+++ app/server.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import application
 
 # api for base route 
 @application.route('/', methods=['GET'])
 def hello():
-    return "Inventory Management server running on localhost:7000"
+    application.logger.info("Inventory Management server running on localhost:7000")
+    return render_template('base.html')
