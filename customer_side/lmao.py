9181105ed49106392960a2bf573bc9768bb8506d
digi11 <digvijay.111200@gmail.com>
2022-03-16 16:46:03 +0530

    changes2

--- customer_side/lmao.py
+++ customer_side/lmao.py
@@ -6,6 +6,3 @@ def hell():
     application.logger.info("Inventory Management server running on localhost:7000")
     return render_template('base.html')
 @application.route('/lmaoo', methods=['GET'])
-def helll():
-    application.logger.info("Inventory Management server running on localhost:7000")
-    return "sex"
