21ee72c821f376336d1e6ebed8957d649d120c3d
kshtijdhara <imkshitijdhara@gmail.com>
2022-03-16 16:54:08 +0530

    lmao

--- customer_side/__init__.py
+++ customer_side/__init__.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, session
+import pyrebase
+from firebase_admin import firestore, credentials, initialize_app
+
+from customer_side import config
+
 
 
 application=Flask(__name__)
+
+
+
+# set up firebase configs for firebase authentication 
+# this uses the config.py file
+firebase_config = config.firebaseConfig
+pb = pyrebase.initialize_app(firebase_config)
+auth = pb.auth()
+
+# set up firestore credentials and database
+# this uses the firestore_config.json file
+cred = credentials.Certificate("app/firestore_config.json")
+default_app = initialize_app(cred)
+db = firestore.client()
+
+
 
 
 from customer_side import (
